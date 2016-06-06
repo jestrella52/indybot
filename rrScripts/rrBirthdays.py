64c2493924846c09e3547a0347e3de462afdf4f4
Brian Keifer <brian@valinor.net>
2016-06-06 17:48:32 -0400

    Add checking for death dates

--- rrScripts/rrBirthdays.py
+++ rrScripts/rrBirthdays.py
@@ -21,15 +21,16 @@ def findDriverID(driverList, last, first):
 con = MySQLdb.connect(read_default_group='indybot', port=3306, db='indybot2', cursorclass=MySQLdb.cursors.DictCursor)
 cur = con.cursor()
 
-query = "SELECT * from driver where dob IS NULL"
+query = "SELECT * from driver where died IS NULL"
 cur.execute(query)
 drivers = cur.fetchall()
 
 bornPattern = re.compile('^<BR><BR><B>Born:</B>\s?([a-zA-Z]+)\s+(\d+),\s(\d+)') #\s?([a-zA-Z]+)\s(\d+), (\d+)$')
-
+diedPattern = re.compile('.*<B>Died:</B>\s?([a-zA-Z]+)\s+(\d+),\s(\d+)')
 
 for driver in drivers:
     driverName = string.replace(driver['first'] + " " + string.replace(driver['last'], ' Sr.', ''), ',', '')
+    # driverName = "Justin Wilson"
     driverAddr = "http://racing-reference.info/driver/" + string.replace(driverName, ' ', '_')
     print driverAddr
     page = requests.get(driverAddr)
@@ -37,10 +38,7 @@ for driver in drivers:
     for line in html:
         match = bornPattern.match(line)
         if match:
-            print "FOUND"
-            print "Month: -" + match.group(1) + '-'
-            print "Day:   -" + match.group(2) + '-'
-            print "Year:  -" + match.group(3) + '-'
+            print "BORN: " + match.group(1) + " " + match.group(2) + ", " + match.group(3)
             if int(match.group(3)) == 2016:
                 print "ERROR!  YEAR IS THIS YEAR - DRIVER ID: " + str(driver['id'])
                 sys.exit()
@@ -53,4 +51,19 @@ for driver in drivers:
             cur.execute(query)
             con.commit()
 
-    time.sleep(5)
+        match2 = diedPattern.match(line)
+        if match2:
+            print "DIED: " + match2.group(1) + " " + match2.group(2) + ", " + match2.group(3)
+            if int(match2.group(3)) == 2016:
+                print "ERROR!  YEAR IS THIS YEAR - DRIVER ID: " + str(driver['id'])
+                sys.exit()
+            stamp2 = datetime.datetime.strptime(match2.group(1) + " " + match2.group(2) + ", " + match2.group(3), "%B %d, %Y")
+            datestamp2 = stamp2.strftime("%Y-%m-%d")
+            query = 'UPDATE driver SET died="'
+            query += datestamp2
+            query += '" where driver.id='
+            query += str(driver['id'])
+            cur.execute(query)
+            con.commit()
+            
+    time.sleep(2)
