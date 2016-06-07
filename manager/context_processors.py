80c61c5ccd7b399caa30bc191d5b3c1094828b21
Brian Keifer <brian@valinor.net>
2016-06-07 16:21:17 -0400

    Add menu item to list current season of races

--- manager/context_processors.py
+++ manager/context_processors.py
@@ -1,9 +1,14 @@
+from datetime import date
+
 from django.db.models import Count
 from .models import Race, Season
 
 def season_processor(request):
     seasons = Season.objects.order_by('-year')
     seasonGroups = Race.objects.values('season_id').annotate(seasonracecount = Count('season_id'))
+
+    currentSeason = Season.objects.filter(year=date.today().year)
+
     seasonRaceCount = {}
     for item in seasonGroups:
         seasonRaceCount[item['season_id']] = item['seasonracecount']
@@ -15,4 +20,5 @@ def season_processor(request):
     return {
         'seasons':seasons,
         'seasonRaceCount':seasonRaceCount,
+        'currentSeason':currentSeason[0].id,
     }
