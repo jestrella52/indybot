68b3bda7718365aa617592337d4d2e9085c7dd2a
Brian Keifer <brian@valinor.net>
2016-06-16 16:31:24 -0400

    - Migrate courseInfo from old script

--- manager/racethread.py
+++ manager/racethread.py
@@ -5,10 +5,11 @@ import calendar
 import time
 
 from django.db import models
+from django.db.models import Count
 from django.core import serializers
 from django.utils import timezone
 
-from .models import Race, Result, ResultType, SessionType
+from .models import Caution, Course, Race, Result, ResultType, SessionType
 
 def sendMail(race, subreddit):
     msg = MIMEText("")
@@ -25,6 +26,7 @@ def compile(race_id, output="reddit"):
     thread = ""
 
     thread += raceInfo(race_id, output)
+    thread += courseInfo(race_id, output)
     thread += winnerList(race_id, output)
 
     return thread
@@ -36,6 +38,78 @@ def link(url, text, output="reddit"):
     else:
         return "<a href=\"" + url + "\">" + text + "</a>"
 
+
+def courseInfo(race_id, output="reddit"):
+    course_id = Race.objects.get(id=race_id).course_id
+    course = Course.objects.select_related('type', 'country', 'fastdriver__country').get(id=course_id)
+
+    cautions = Caution.objects.select_related('race__course').filter(race__course_id=course_id)
+
+    totalCautions = 0
+    totalCautionLaps = 0
+    raceIDs = set()
+
+    for caution in cautions:
+        raceIDs.add(caution.race_id)
+        totalCautions = totalCautions + 1
+        totalCautionLaps = totalCautionLaps + (caution.endLap - caution.startLap + 1)
+
+    if output == "reddit":
+        bs = "**"
+        be = "**"
+        nl = "\n\n"
+        hr = "***"
+        rs = ""
+        re = ""
+        cs = "|"
+    else:
+        bs = "<strong>"
+        be = "</strong>"
+        nl = "<br>"
+        hr = "<hr>"
+        rs = "<tr><td>"
+        re = "</td></tr>"
+        cs = "</td><td>"
+
+    courseTable  = hr + nl + bs + "CIRCUIT INFORMATION: " + link(course.url, course.name, output) + be + nl
+
+    if output == "reddit":
+        courseTable += "||||\n:--|:--|:--\n"
+    else:
+        courseTable += "<table>"
+
+    courseTable += rs + "Length:" + cs + cs + str(course.length) + " miles" + re + "\n"
+    courseTable += rs + "Type:" + cs + cs + course.type.type + re + "\n"
+    courseTable += rs + "Location:" + cs + cs + course.location + " [](/" + course.country.iso + ")" + re + "\n"
+    courseTable += rs + "Fast Lap:" + cs + cs + str(course.fastlap) + " mph by " + course.fastdriver.first + " " + course.fastdriver.last + " [](/" + course.fastdriver.country.iso + ") in " + str(course.fastyear) + re + "\n"
+    courseTable += rs + "Coordinates:" + cs + cs + link("https://maps.google.com/?q=" + course.gps, course.gps, output) + re + "\n"
+    courseTable += rs + "Caution periods:" + cs + cs + "Average of " + str( round(float(totalCautions) / float(len(raceIDs)), 1)  ) + " caution periods per race (over last " + str(len(raceIDs)) + " races)"+ re + "\n"
+    courseTable += rs + "Caution lengths:" + cs + cs + "Average of " + str( round(float(totalCautionLaps) / float(totalCautions), 1) ) + " laps per caution period (over last " + str(len(raceIDs)) + " races)" + re + "\n"
+    courseTable += "</table>"
+
+    return courseTable
+    # query = """SELECT race_id, COUNT(race_id)
+    #            FROM caution
+    #            WHERE race_id IN
+    #             (SELECT id
+    #              FROM race
+    #              WHERE race.course_id = """ + str(course) + """
+    #              AND race.green < CURRENT_DATE)
+    #            GROUP BY race_id"""
+    #
+    # courseData = con.cursor()
+    # courseData.execute(query)
+    # rows = courseData.fetchall()
+    #
+    # cautions = 0
+    # races = 0
+    # for row in rows:
+    #     races = races + 1
+    #     cautions = cautions + row[1]
+    #
+    # courseTable += "Average # of Caution Periods:||" + str(round(float(cautions)/float(races), 1)) + " (over last " + str(races) + " races)\n\n"
+    #
+    # return courseTable
 
 def raceInfo(race_id, output="reddit"):
     race = Race.objects.select_related('course', 'start').get(id=race_id)
