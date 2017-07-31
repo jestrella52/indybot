f2aea5fc971389c1921305af2fdd85fe19ed5bb2
Brian Keifer <brian@valinor.net>
2017-07-30 23:55:00 -0400

    Avoid logs going to celery logfiles

--- manager/support.py
+++ manager/support.py
@@ -1,11 +1,10 @@
 import logging
 from django.conf import settings
 
-logging.basicConfig(level=logging.INFO,
-                    filename=settings.INDYBOT_LOGFILE,
-                    format='%(asctime)s %(message)s')
-
 def logit(message):
+    logging.basicConfig(level=logging.INFO,
+                        filename=settings.INDYBOT_LOGFILE,
+                        format='%(asctime)s %(message)s')
     logging.info(str(message))
     # with open(settings.INDYBOT_LOGFILE, "a") as myfile:
     #     myfile.write(message + "\n")
