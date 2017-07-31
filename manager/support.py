7b1f58178c44182b4fb48987e85b48258feb049b
Brian Keifer <brian@valinor.net>
2017-07-31 00:01:06 -0400

    Take 392

--- manager/support.py
+++ manager/support.py
@@ -1,10 +1,12 @@
 import logging
 from django.conf import settings
 
+logging.basicConfig(level=logging.INFO,
+                    filename=settings.INDYBOT_LOGFILE,
+                    format='[%(asctime)s] %(message)s')
+logger = logging.getLogger('indybot')
+
 def logit(message):
-    logging.basicConfig(level=logging.INFO,
-                        filename=settings.INDYBOT_LOGFILE,
-                        format='%(asctime)s %(message)s')
-    logging.info(str(message))
+    logger.info(str(message))
     # with open(settings.INDYBOT_LOGFILE, "a") as myfile:
     #     myfile.write(message + "\n")
