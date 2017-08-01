58e0a8fe00a9b817749d4ac8eacda47160a827bb
Brian Keifer <brian@valinor.net>
2017-08-01 13:29:15 -0400

    Bail on using logging module for now

--- manager/support.py
+++ manager/support.py
@@ -1,12 +1,5 @@
-import logging
 from django.conf import settings
 
-logging.basicConfig(level=logging.INFO,
-                    filename=settings.INDYBOT_LOGFILE,
-                    format='[%(asctime)s] %(message)s')
-logger = logging.getLogger('indybot')
-
 def logit(message):
-    logger.info(str(message))
-    # with open(settings.INDYBOT_LOGFILE, "a") as myfile:
-    #     myfile.write(message + "\n")
+    with open(settings.INDYBOT_LOGFILE, "a") as myfile:
+        myfile.write(str(message) + "\n")
