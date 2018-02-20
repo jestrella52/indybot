adb7c40b48975ff0d883147e4b98e4a3103afab9
Brian Keifer <brian@valinor.net>
2018-02-20 14:50:27 -0500

    Handle when no season object exists for this year

--- manager/context_processors.py
+++ manager/context_processors.py
@@ -3,11 +3,16 @@ from datetime import date
 from django.db.models import Count
 from .models import Race, Season
 
+from .support import logit
+
 def season_processor(request):
     seasons = Season.objects.order_by('-year')
     seasonGroups = Race.objects.values('season_id').annotate(seasonracecount = Count('season_id'))
 
     currentSeason = Season.objects.filter(year=date.today().year)
+    if not currentSeason:
+        currentSeason = Season.objects.order_by('-year')[:1]
+        logit("WARNING - No season defined for the current year.  Using " + str(currentSeason[0].year) + "!" )
 
     seasonRaceCount = {}
     for item in seasonGroups:
