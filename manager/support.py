b8c8a928777741cec61b2622054a56da5fc2b341
Brian Keifer <brian@valinor.net>
2018-02-20 13:12:49 -0500

    Add more information to log messages

--- manager/support.py
+++ manager/support.py
@@ -1,5 +1,14 @@
+import datetime
+import inspect
+
 from django.conf import settings
 
 def logit(message):
+    stamp = datetime.datetime.now().strftime("%Y-%d-%m %H:%M:%S")
+    try:
+        caller = inspect.stack()[1][3]
+    except:
+        caller = "unknown"
+
     with open(settings.INDYBOT_LOGFILE, "a") as myfile:
-        myfile.write(str(message) + "\n")
+        myfile.write(stamp + " - " + caller + " - " + str(message) + "\n")
